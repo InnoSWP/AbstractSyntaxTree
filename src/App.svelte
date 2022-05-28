ae4748cdee55447f7ab85641e372928c3ff35b95
geffk2 <arsmax3@gmail.com>
2022-05-28 11:39:47 +0300

    Added CodeMirror component `tree` prop

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,32 @@
   import Button from "./Button.svelte";
   import Logo from "./Logo.svelte";
   import Tailwind from "./Tailwind.svelte";
+  import CodeMirror from "./CodeMirror.svelte";
+
+  let view
+  let tree
+  $: console.log(tree)
 </script>
 
 <Tailwind />
-<main class="container mx-auto">
-  <div class="bg-gray-200 px-4 py-2 rounded">Hello Tailwind!</div>
-  <Button>A Button</Button>
-  <Logo name={"A Logo"} />
-</main>
+
+<div class="h-full flex items-stretch">
+  <CodeMirror classes="h-full w-1/3" bind:view bind:tree/>
+  
+  <div class="h-full w-1/3">
+    <div class="bg-gray-200 px-4 py-2 rounded">Hello Tailwind!</div>
+    <Button>A Button</Button>
+    <Logo name={"A Logo"} />
+  </div>
+  <div class="h-full w-1/3">
+    <div class="bg-gray-200 px-4 py-2 rounded">Hello Tailwind!</div>
+    <Button>A Button</Button>
+    <Logo name={"A Logo"} />
+  </div>
+</div>
+
+<style lang="postcss">
+  :global(html, body) {
+    height: 100%;
+  }
+</style>
---
+++ src/CodeMirror.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { onMount } from 'svelte'
+    import { EditorState, basicSetup } from '@codemirror/basic-setup'
+    import { EditorView, keymap, ViewUpdate } from '@codemirror/view'
+    import { indentWithTab } from '@codemirror/commands'
+    import { javascript } from '@codemirror/lang-javascript'
+    import { parseScript, parseModule } from 'esprima'
+
+    onMount(() => {
+        createEditor()
+    })
+
+    export let view: EditorView = null
+    export let classes: string = ""
+
+    let root
+    export let tree = null
+
+    function updateHandle(v: ViewUpdate) {
+        if(v.docChanged) {
+            try {
+                tree = parseModule(view.state.doc.toString())
+            } catch {}
+        }
+    }
+
+    function createEditor() {
+        view = new EditorView({
+            state: EditorState.create({
+                extensions: [
+                    EditorView.updateListener.of(updateHandle),
+                    basicSetup,
+                    javascript(),
+                    keymap.of([indentWithTab]),
+                ],
+                doc: 'test',
+            }),
+            parent: root,
+        })
+    }
+
+</script>
+
+<div bind:this={root} class={classes} />
+
+<style lang="postcss">
+    :global(.cm-editor) {
+        height:100%;
+        width: 100%;
+    }
+</style>
