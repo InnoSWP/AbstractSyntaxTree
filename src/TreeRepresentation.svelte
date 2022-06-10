79eccbcd993dcdc993c7768305d02a8a1c9e65d9
geffk2 <arsmax3@gmail.com>
2022-06-10 19:46:13 +0300

    Make the tree a little more compact

--- src/TreeRepresentation.svelte
+++ src/TreeRepresentation.svelte
@@ -4,7 +4,7 @@
     export let tree: Program = null;
 </script>
 
-<div class="tree-representation overflow-scroll h-full">
+<div class="tree-representation overflow-scroll h-full text-xs">
     <ul>
         {#if tree != null}
             <TreeObject obj={tree}/>
@@ -25,10 +25,6 @@
         @apply flex-1 cursor-default font-mono box-border;
     }
 
-    :global(span) {
-        @apply text-sm;
-    }
-
     :global(li.entry) {
         @apply m-0 p-0 relative list-none;
 
@@ -37,7 +33,7 @@
     }
 
     :global(.entry.togglable::before) {
-        @apply absolute -left-4 top-1.5 w-3 h-3;
+        @apply absolute -left-4 top-0.5 w-3 h-3;
         content: "";
         background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' d='M9 5l7 7-7 7' /%3E%3C/svg%3E");
     }
@@ -79,7 +75,7 @@
         @apply block pl-5;
         margin-inline-start: 0px;
         margin-inline-end: 0px;
-        padding-inline-start: 30px;
+        padding-inline-start: 20px;
 
         min-width: 300px;
         width: fit-content;
