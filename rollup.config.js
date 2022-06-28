7f023914850532f8a3d0b6db8273f5d6b9c5db0d
Dirakon <609700zxc@mail.ru>
2022-06-28 23:16:04 +0300

    configured .js files with standard

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,47 @@
-import svelte from "rollup-plugin-svelte";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
-import sveltePreprocess from "svelte-preprocess";
-import typescript from "@rollup/plugin-typescript";
-import css from "rollup-plugin-css-only";
+import svelte from 'rollup-plugin-svelte'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import livereload from 'rollup-plugin-livereload'
+import { terser } from 'rollup-plugin-terser'
+import sveltePreprocess from 'svelte-preprocess'
+import typescript from '@rollup/plugin-typescript'
+import css from 'rollup-plugin-css-only'
 
-const preprocessOptions = require("./svelte.config").preprocessOptions;
-const production = !process.env.ROLLUP_WATCH;
+const preprocessOptions = require('./svelte.config').preprocessOptions
+const production = !process.env.ROLLUP_WATCH
 
-function serve() {
-  let server;
+function serve () {
+  let server
 
-  function toExit() {
-    if (server) server.kill(0);
+  function toExit () {
+    if (server) server.kill(0)
   }
 
   return {
-    writeBundle() {
-      if (server) return;
-      server = require("child_process").spawn(
-        "npm",
-        ["run", "start", "--", "--dev"],
+    writeBundle () {
+      if (server) return
+      server = require('child_process').spawn(
+        'npm',
+        ['run', 'start', '--', '--dev'],
         {
-          stdio: ["ignore", "inherit", "inherit"],
-          shell: true,
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true
         }
-      );
+      )
 
-      process.on("SIGTERM", toExit);
-      process.on("exit", toExit);
-    },
-  };
+      process.on('SIGTERM', toExit)
+      process.on('exit', toExit)
+    }
+  }
 }
 
 export default {
-  input: "src/main.ts",
+  input: 'src/main.ts',
   output: {
     sourcemap: true,
-    format: "iife",
-    name: "app",
-    file: `${process.env.dir}/build/bundle.js`,
+    format: 'iife',
+    name: 'app',
+    file: `${process.env.dir}/build/bundle.js`
   },
   plugins: [
     svelte({
@@ -50,24 +50,24 @@ export default {
         ...preprocessOptions
       }),
       compilerOptions: {
-        dev: !production,
-      },
+        dev: !production
+      }
     }),
-    css({ output: "bundle.css" }),
+    css({ output: 'bundle.css' }),
     resolve({
       browser: true,
-      dedupe: ["svelte"],
+      dedupe: ['svelte']
     }),
     commonjs(),
     typescript({
       sourceMap: !production,
-      inlineSources: !production,
+      inlineSources: !production
     }),
     !production && serve(),
-    !production && livereload("public"),
-    production && terser(),
+    !production && livereload('public'),
+    production && terser()
   ],
   watch: {
-    clearScreen: false,
-  },
-};
+    clearScreen: false
+  }
+}
